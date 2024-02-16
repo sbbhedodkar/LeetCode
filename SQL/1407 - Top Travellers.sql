1a2b700a1ba56f7d4e5210d0d39e50e6c6613a84
sbbhedodkar <sbbhedodkar@gmail.com>
2024-02-16 23:02:34 +0530

    1407 - Top Travellers Soultion

---
+++ SQL/1407 - Top Travellers.sql
@@ -0,0 +1,11 @@
+SELECT
+    name,
+    IFNULL(SUM(distance), 0) AS travelled_distance
+FROM
+    Users AS u
+    LEFT JOIN Rides AS r ON u.id = r.user_id
+GROUP BY
+    u.id
+ORDER BY
+    2 DESC,
+    1;
